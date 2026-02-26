aead58d8d2b241535df0f603385955045d45206c
quinnmpaddock <qmcconnel@gmail.com>
2026-02-25 17:54:52 -0800

    cleaning up code

--- src/core_classes.py
+++ src/core_classes.py
@@ -171,9 +171,7 @@ class GraphRAGStore(Neo4jPropertyGraphStore):
             ),
             ChatMessage(role="user", content=text),
         ]
-        # print("TEST: constructed chat message!")
         llm = Groq(model="meta-llama/llama-4-scout-17b-16e-instruct")
-        # print("TEST: specifying llm")
         response = llm.chat(messages)
 
         clean_response = re.sub(r"^assistant:\s*", "", str(response)).strip()
@@ -184,42 +182,10 @@ class GraphRAGStore(Neo4jPropertyGraphStore):
         """Sends cypher commands to the neo4j database"""
         if params is None:
             params = {}
-        # with self._driver.session() as session:
-        #     result = session.run(query, params)
-        #     return [record.data() for record in result]
         records, _, _ = self._driver.execute_query(
             query, params=params, database_=self.graph_name
         )
         return [record.data() for record in records]
-
-    # def build_communities(self):
-    #     print("Building communities")
-    #     """Builds communities from the graph and summarizes them."""
-    #     nx_graph = self._create_nx_graph()
-    #     community_hierarchical_clusters = hierarchical_leiden(
-    #         nx_graph, max_cluster_size=self.max_cluster_size
-    #     )
-    #     self.entity_info, community_info = self._collect_community_info(
-    #         nx_graph, community_hierarchical_clusters
-    #     )
-    #     self._summarize_communities(community_info)
-    #
-    # def _create_nx_graph(self):
-    #     print("creating nx graph")
-    #     """Converts internal graph representation to NetworkX graph."""
-    #     nx_graph = nx.Graph()
-    #     triplets = self.get_triplets()
-    #     for entity1, relation, entity2 in triplets:
-    #         nx_graph.add_node(entity1.name)
-    #         nx_graph.add_node(entity2.name)
-    #         nx_graph.add_edge(
-    #             relation.source_id,
-    #             relation.target_id,
-    #             relationship=relation.label,
-    #             description=relation.properties["relationship_description"],
-    #             source=relation.properties.get("title", "Unknown Source"),
-    #         )
-    #     return nx_graph
 
     def build_communities(self):
         """Builds communities from the graph and persists them to the neo4j database"""
@@ -265,32 +231,6 @@ class GraphRAGStore(Neo4jPropertyGraphStore):
         self._run_cypher(f"CALL gds.graph.drop('{self.graph_name}') YIELD graphName")
         self._collect_community_info()
 
-    # def _collect_community_info(self, nx_graph, clusters):
-    #     print("collecting community info")
-    #     """
-    #     Collect information for each node based on their community,
-    #     allowing entities to belong to multiple clusters.
-    #     """
-    #     entity_info = defaultdict(set)
-    #     community_info = defaultdict(list)
-    #
-    #     for item in clusters:
-    #         node = item.node
-    #         cluster_id = item.cluster
-    #
-    #         # Update entity_info
-    #         entity_info[node].add(cluster_id)
-    #
-    #         for neighbor in nx_graph.neighbors(node):
-    #             edge_data = nx_graph.get_edge_data(node, neighbor)
-    #             if edge_data:
-    #                 detail = f"{node} -> {neighbor} -> {edge_data['relationship']} -> {edge_data['description']} [Source: {edge_data['source']}]"
-    #                 community_info[cluster_id].append(detail)
-    #
-    #     # Convert sets to lists for easier serialization if needed
-    #     entity_info = {k: list(v) for k, v in entity_info.items()}
-    #
-    #     return dict(entity_info), dict(community_info)
     def _collect_community_info(self):
         """
         Collect information for each node based on their community,
